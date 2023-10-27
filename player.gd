6285529129df73ea527cbe1bfcd64f9a492a3e51
Toby Clarke <teeohbee@gmail.com>
2023-10-27 21:55:19 +0100

    Implement shooting and thrust animation

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 @export var engine_power = 500
 @export var spin_power = 4000
+@export var bullet_scene : PackedScene
 
 var thrust = Vector2.ZERO
 var rotation_direction = 0
@@ -13,9 +14,19 @@ func get_input():
 	thrust = Vector2.ZERO
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
+		$Thruster.show()
+	else:
+		$Thruster.hide()
 	rotation_direction = Input.get_axis("rotate_left", "rotate_right")
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
 	constant_force = thrust
 	constant_torque = rotation_direction * spin_power
 	
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	get_tree().root.add_child(bullet)
+	bullet.start($Muzzle.global_transform)
