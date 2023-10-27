2f1c1a4cdec2a54e1ecae12a63addbc5700ad4ed
Toby Clarke <teeohbee@gmail.com>
2023-10-27 21:27:10 +0100

    Initial commit - Sets up the project - Adds player rigid body and sprite - Adds player movement

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+@export var engine_power = 500
+@export var spin_power = 4000
+
+var thrust = Vector2.ZERO
+var rotation_direction = 0
+
+func _process(delta):
+	get_input()
+
+func get_input():
+	thrust = Vector2.ZERO
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_direction = Input.get_axis("rotate_left", "rotate_right")
+
+func _physics_process(delta):
+	constant_force = thrust
+	constant_torque = rotation_direction * spin_power
+	
