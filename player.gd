e85afd49dad55ca44fff380bde6468789440c7a2
Toby Clarke <teeohbee@gmail.com>
2023-10-29 19:48:58 +0000

    Add main scene and two players - Screen size set to 1080p - Players start in opposite corners of the screen

--- player.gd
+++ player.gd
@@ -3,9 +3,16 @@ extends RigidBody2D
 @export var engine_power = 500
 @export var spin_power = 4000
 @export var bullet_scene : PackedScene
+@export var starting_position = 0.1
 
 var thrust = Vector2.ZERO
 var rotation_direction = 0
+
+func _ready():
+	var screen_size = get_viewport_rect().size
+	position = screen_size * starting_position
+	if starting_position > 0.5:
+		rotation_degrees = 180
 
 func _process(delta):
 	get_input()
