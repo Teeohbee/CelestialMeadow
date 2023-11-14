118d0e7bbfb592c613a6c42a44bb11196f7e3071
Toby Clarke <teeohbee@gmail.com>
2023-11-14 16:25:57 +0000

    Adjust asteroid spawning and speed - Increased chance of faster asteroids - Increased number of starting asteroids - Asteroids respawn when fewer than 10 remain

--- main.gd
+++ main.gd
@@ -5,12 +5,18 @@ var screensize
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
-	for i in 5:
+	for i in 10:
 		spawn_asteroid()
 
 func spawn_asteroid():
 	$AsteroidPath/AsteroidSpawn.progress = randi()
-	var velocity = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50, 125)
+	var velocity = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(100, 200)
 	var asteroid = asteroid_scene.instantiate()
 	asteroid.start($AsteroidPath/AsteroidSpawn.position, velocity)
 	call_deferred("add_child", asteroid)
+
+func _on_asteroid_timer_timeout():
+	var asteroids = get_tree().get_nodes_in_group("asteroids")
+	if asteroids.size() < 10:
+		for i in 2:
+			spawn_asteroid()
