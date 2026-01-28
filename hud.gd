1d0175f20876e956fdcfbff3fc22b03f8f938745
Toby Clarke <teeohbee@gmail.com>
2026-01-28 14:48:39 +0000

    Fix HUD to always load all 4 labels and hide inactive ones

--- hud.gd
+++ hud.gd
@@ -3,9 +3,13 @@ extends CanvasLayer
 var player_labels = []
 
 func _ready():
-	for i in GameState.num_players:
+	# Get all 4 labels
+	for i in range(4):
 		var label = get_node("MarginContainer/VBoxContainer/Player%dLabel" % (i + 1))
 		player_labels.append(label)
+		# Hide labels for inactive players
+		if i >= GameState.num_players:
+			label.hide()
 
 func update_lives(player_number: int, lives: int):
 	if player_number < player_labels.size():
